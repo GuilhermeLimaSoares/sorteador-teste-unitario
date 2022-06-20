f8f93a1ad3b25b11f74be211dee15d1c930b95ae
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2022-06-19 22:59:13 -0300

    Finaliza curso de teste

---
+++ src/componentes/ListaParticipantes.tsx
@@ -0,0 +1,19 @@
+import { useListaDeParticipantes } from "../state/hook/useListaDeParticipantes";
+
+
+const ListaParticipantes = () => {
+    const participantes: string[] = useListaDeParticipantes();
+    return (
+        <ul>
+            { participantes
+                .map( participante => 
+                    <li 
+                        key={participante}>
+                        {participante}
+                    </li>
+                )}
+        </ul>
+    )
+}
+
+export default ListaParticipantes;
